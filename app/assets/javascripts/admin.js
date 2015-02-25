e064ef424a259f5e159e79e9b9d00219b1ddc3a9
complitech <admin@complitech.net>
2015-02-25 19:00:51 +0530

    add the template

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,27 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery-ui-1.10.3.custom.min
+//= require less-1.5.0.min
+//= require bootstrap.min
+//= require bootstrap 
+//= require bootstrap-switch
+//= require jquery.accordion
+//= require bootstrap-typeahead
+//= require jpreLoader.min
+//= require preloader-delighted
+//= require jquery.easy-pie-chart
+//= require core.js
+//= require_tree .
---
+++ app/controllers/admin/base_controller_controller.rb
@@ -0,0 +1,4 @@
+class Admin::BaseControllerController < ApplicationController
+layout 'admin'
+
+end
