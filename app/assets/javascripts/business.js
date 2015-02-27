b3e108a0ff61f8ed80e712fc8f9fb523901b24a2
deep-complitech <deep@deep.cs>
2015-02-27 17:01:33 +0530

    added login page layout to app and redirect it to user's role

---
+++ app/assets/javascripts/business.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery-ui-1.10.3.custom.min
+//= require less-1.5.0.min
+//= require bootstrap.min
+//= require bootstrap 
+//= require bootstrap-switch
+//= require jquery.accordion
+//= require bootstrap-typeahead
+//= require jpreLoader.min
+//= require preloader-delighted
+//= require jquery.easy-pie-chart
+//= require core.js
+// require_tree .
