28e9d7346e8625b78eff161b5cba4abf81c832e0
complitech <admin@complitech.net>
2015-03-04 13:08:35 +0530

    add enum in offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ActiveRecord::Base
 	belongs_to   :store
 	has_many :offer_details , dependent: :destroy
+	#enumaration 
+	enum offer_for: [ :new_user, :existing_user,:both_user ]
 end
