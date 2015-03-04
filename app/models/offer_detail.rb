6896756a9f541a17fa8d87ff40767f455e34edd1
complitech <admin@complitech.net>
2015-03-04 12:35:32 +0530

    add the visitor tableapp/controllers/api/v1/indorse/offers_controller.rb

--- app/models/offer_detail.rb
+++ app/models/offer_detail.rb
@@ -1,4 +1,6 @@
 class OfferDetail < ActiveRecord::Base
   belongs_to :customer,class_name: "Customer", foreign_key: :user_id
   belongs_to :offer
+	#validation  
+  validates :offer_id, uniqueness: {scope: :user_id}, presence: true
 end
---
+++ app/models/visitor_detail.rb
@@ -0,0 +1,4 @@
+class VisitorDetail < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :store
+end
