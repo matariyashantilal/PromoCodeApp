dfe6595f11c8af60894e7ab31526046ea8ff84c3
complitech <admin@complitech.net>
2015-03-16 10:24:59 +0530

    add authentication validation in base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,15 @@
 class Admin::BaseController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!,:check_for_admin
 	layout 'admin'
 	
+	def check_for_admin
+		if user_signed_in?
+	  	if !current_user.admin?
+	  		redirect_to edit_business_business_path(current_user)
+	  	end
+  	else
+  		redirect_to new_user_session_path
+  	end
+	end
+
 end
--- app/controllers/business/base_controller.rb
+++ app/controllers/business/base_controller.rb
@@ -1,5 +1,15 @@
 class Business::BaseController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!,:check_for_admin
+
 	
 	layout 'business'
+	def check_for_admin
+		if user_signed_in?
+	  	if !current_user.business?
+	  		redirect_to admin_root_path
+	  	end
+  	else
+  		redirect_to new_user_session_path
+  	end
+	end
 end
