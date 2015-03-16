a25d371f277e081b3e2d549c890d2d7dc248dd5e
complitech <admin@complitech.net>
2015-03-16 14:48:22 +0530

    change the add_visit api and set return data same as near by store

--- app/controllers/api/v1/indorse/offers_controller.rb
+++ app/controllers/api/v1/indorse/offers_controller.rb
@@ -49,32 +49,73 @@ class Api::V1::Indorse::OffersController < Api::V1::BaseController
         puts("=====#{@offers.count}")
         if @offers.present?
             puts("==== offer present ======")
-            @valid_offers = []
             @offers.each do |offer|
                 puts("==#{offer.inspect}")
                 @visit_offer_count=VisitorDetail.get_visitor_detail(@current_user.id,@store.id,offer.created_at).count
-                puts("==#{@visit_offer_count}")
-                #if offer.punch_count <= @visit_offer_count
-                    puts("====== vvvvvvvalid loop")
-                    @valid_offers << offer
                     @offer_details=OfferDetail.new(user_id: @current_user.id,offer_id: offer.id)
                     @offer_details.save
-                #end
             end  
-            puts("=====valid offers ==========")
-            puts(@valid_offers)
-            if !@valid_offers.present?
-              render_json({:errors => "Offer not found. Please enter a valid offer first."}.to_json)
-            end
-        else 
-            render_json({:errors => "Offer not found. Please enter a valid offer first."}.to_json)
-        end 
+        end
+         puts("=======@store.inspect")
+         puts("=======@store.latitude")
+
+        @stores   = Store.near([@store.latitude, @store.longitude], 100).joins(:offers).where("offer_expire_on >= ?",Date.today.beginning_of_day).includes(:ibeacons).uniq
+        
+       if !@stores.present?
+          render_json({:result=>{:messages =>"No Stores Present.",:rstatus=>1, :errorcode =>""},:data=>{}}.to_json)    
+        end
+     
     else
         render_json({:errors => "No user found with authentication_token = #{params[:authentication_token]}"}.to_json)
     end 
   end
 
+  # create at deep sir
+  # def add_visits
+  #   @store=Store.find(params[:store_id])
+  #  if @store.present? && @current_user.present?
+  #       visit_detail=VisitorDetail.new(store_id: @store.id,user_id: @current_user.id)
+  #       @visit_count=VisitorDetail.get_visitor_detail(@current_user.id,@store.id).count
+  #       check_new=VisitorDetail.check_for_new(@current_user.id,@store.id)
+  #       @is_save=VisitorDetail.check_for_today_visit(@current_user.id,@store.id)  
+  #       if @is_save == 0 
+  #         if !visit_detail.save
+  #           render_json({:result=>{:messages =>@visit_detail.errors.full_messages,:rstatus=>0, :errorcode => 404}}.to_json)
+  #         end
+  #       end        
+  #       storeOffer = Offer.all.get_non_expired_offers.where("store_id = ?", @store.id)
+  #       puts("=====storeOffer=====")
+  #       puts(storeOffer)
+  #       @offers = check_new != 0 ? storeOffer.existing_user_offer.where("punch_count <= ? ",@visit_count) : storeOffer.new_user_offer.where("punch_count >= ? ",@visit_count)
+  #       puts("=====#{@offers.count}")
+  #       if @offers.present?
+  #           puts("==== offer present ======")
+  #           @valid_offers = []
+  #           @offers.each do |offer|
+  #               puts("==#{offer.inspect}")
+  #               @visit_offer_count=VisitorDetail.get_visitor_detail(@current_user.id,@store.id,offer.created_at).count
+  #               puts("==#{@visit_offer_count}")
+  #               #if offer.punch_count <= @visit_offer_count
+  #                   puts("====== vvvvvvvalid loop")
+  #                   @valid_offers << offer
+  #                   @offer_details=OfferDetail.new(user_id: @current_user.id,offer_id: offer.id)
+  #                   @offer_details.save
+  #               #end
+  #           end  
+  #           puts("=====valid offers ==========")
+  #           puts(@valid_offers)
+  #           if !@valid_offers.present?
+  #             render_json({:errors => "Offer not found. Please enter a valid offer first."}.to_json)
+  #           end
+  #       else 
+  #           render_json({:errors => "Offer not found. Please enter a valid offer first."}.to_json)
+  #       end 
+  #   else
+  #       render_json({:errors => "No user found with authentication_token = #{params[:authentication_token]}"}.to_json)
+  #   end 
+  # end
 
+  # create at shantilal
   # def add_visits
   #   @store=Store.find(params[:store_id])
   #   if @store.present? && @current_user.present?
